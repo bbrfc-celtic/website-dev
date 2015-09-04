5775c2fc1ef0448a664dc4ca36475886069a006b
Carlos Requena López <crequena@ulb.ac.be>
2015-09-05 00:58:35 +0200

    middleman deploy remote update

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ end
 activate :deploy do |deploy|
   deploy.method = :git
   # Optional Settings
-  deploy.remote   = 'publish' # make sure 'publish' points to good github repo.
+  deploy.remote   = 'git@github.com:bbrfc-celtic/bbrfc-celtic.github.io.git'
   deploy.branch   = 'master' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # =>  deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
