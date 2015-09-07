eb9bae6e2ed7685bc0febed4b519842a540549f1
Carlos Requena Lopez <crequena@ulb.ac.be>
2015-09-07 15:49:56 +0200

    men forms added with links and CNAME

--- config.rb
+++ config.rb
@@ -128,4 +128,3 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # =>  deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
-activate :alias
