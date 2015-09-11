4339320862319bb2e3669051ccac9d5f4b997df6
Carlos Requena <crequena@ulb.ac.be>
2015-09-11 16:06:16 +0200

    testing build image with ORG mode and trying to deploy with travis

--- config.rb
+++ config.rb
@@ -123,7 +123,7 @@ end
 activate :deploy do |deploy|
   deploy.method = :git
   # Optional Settings
-  deploy.remote   = 'https://${GH_TOKEN}@github.com/bbrfc-celtic/bbrfc-celtic.github.io'
+  deploy.remote   = 'https://${GH_TOKEN}@github.com/bbrfc-celtic/bbrfc-celtic.github.io.git'
   deploy.branch   = 'master' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # =>  deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
